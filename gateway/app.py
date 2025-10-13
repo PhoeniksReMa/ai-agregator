8534c6c0a67caf6646925e3f8d4e57e070edce9e
MAKSIM REZNICHENKO <maksimrez96@gmail.com>
2025-10-13 03:29:26 +0300

    Вернул все модели

--- gateway/app.py
+++ gateway/app.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Local AI Gateway", lifespan=lifespan)
 app.include_router(xtts.router)
 app.include_router(wisper.router)
 app.include_router(ollama.router)
-# app.include_router(comfy.router)
+app.include_router(comfy.router)
 
 @app.get("/health")
 async def health():
